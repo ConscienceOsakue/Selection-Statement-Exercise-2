e581cd0c8c744a076e37073f68cc66481b3d2e73
conscience osakue <consciencenosa@gmail.com>
2023-11-15 18:12:06 -0600

    first commint.

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("What is your favorite school subject?");
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is a tough subject.");
+                    break;
+                case "English":
+                    Console.WriteLine("Englist is a tough subject.");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is a tough subject.");
+                    break;
+                case "Physics":
+                    Console.WriteLine("Physics is a tough subject.");
+                    break;
+                case "Biology":
+                    Console.WriteLine("Biology is a tough subject.");
+                    break;
+                default: 
+                    Console.WriteLine($"Oh Wow! I haven't taken that subject before, {subject} sounds fun.");
+                    break;
+            }
         }
     }
 }
